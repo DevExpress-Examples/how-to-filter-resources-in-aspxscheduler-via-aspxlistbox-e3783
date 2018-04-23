15118ab3b7c29adab4316073bb8db6b595318b28
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxScheduler;
+using DevExpress.XtraScheduler;
+
+public partial class _Default : System.Web.UI.Page {
+    private int lastInsertedAppointmentId;
+
+    private string defaultResourcesSelectCommand = "SELECT [ID], [Model] FROM [Cars]";
+
+    protected void Page_Init(object sender, EventArgs e) {
+        string param = Page.Request["__CALLBACKPARAM"];
+
+        if (!IsPostBack)
+            Session["resourcesSelectCommand"] = defaultResourcesSelectCommand;
+
+        if (!string.IsNullOrEmpty(param) && param.Contains("FLTRES|")) {
+            string resources = param.Substring(param.IndexOf('|') + 1);
+            if (resources.Length > 0)
+                Session["resourcesSelectCommand"] = string.Format("SELECT [ID], [Model] FROM [Cars] WHERE [ID] IN ({0})", resources);
+            else
+                Session["resourcesSelectCommand"] = defaultResourcesSelectCommand;
+        }
+
+        SqlDataSourceResources.SelectCommand = Session["resourcesSelectCommand"].ToString();
+    }
+
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!IsPostBack) {
+            ASPxScheduler1.Start = new DateTime(2008, 7, 12);
+        }
+    }
+
+    protected void ASPxScheduler1_BeforeExecuteCallbackCommand(object sender, SchedulerCallbackCommandEventArgs e) {
+        if (e.CommandId.Contains("FLTRES"))
+            ASPxScheduler1.ApplyChanges(ASPxSchedulerChangeAction.NotifyResourceIntervalChanged);
+    }
+
+    protected void ASPxScheduler1_AppointmentRowInserting(object sender, DevExpress.Web.ASPxScheduler.ASPxSchedulerDataInsertingEventArgs e) {
+        e.NewValues.Remove("ID");
+    }
+
+    protected void SqlDataSourceAppointments_Inserted(object sender, SqlDataSourceStatusEventArgs e) {
+        SqlConnection connection = (SqlConnection)e.Command.Connection;
+
+        using(SqlCommand command = new SqlCommand("SELECT IDENT_CURRENT('CarScheduling')", connection)) {
+            lastInsertedAppointmentId = Convert.ToInt32(command.ExecuteScalar());
+        }
+    }
+
+    protected void ASPxScheduler1_AppointmentRowInserted(object sender, DevExpress.Web.ASPxScheduler.ASPxSchedulerDataInsertedEventArgs e) {
+        e.KeyFieldValue = lastInsertedAppointmentId;
+    }
+
+    protected void ASPxScheduler1_AppointmentsInserted(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e) {
+        Appointment apt = (Appointment)e.Objects[0];
+        ASPxSchedulerStorage storage = (ASPxSchedulerStorage)sender;
+        storage.SetAppointmentId(apt, lastInsertedAppointmentId);
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,66 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data.SqlClient
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxScheduler
+Imports DevExpress.XtraScheduler
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private lastInsertedAppointmentId As Integer
+
+	Private defaultResourcesSelectCommand As String = "SELECT [ID], [Model] FROM [Cars]"
+
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim param As String = Page.Request("__CALLBACKPARAM")
+
+		If (Not IsPostBack) Then
+			Session("resourcesSelectCommand") = defaultResourcesSelectCommand
+		End If
+
+		If (Not String.IsNullOrEmpty(param)) AndAlso param.Contains("FLTRES|") Then
+			Dim resources As String = param.Substring(param.IndexOf("|"c) + 1)
+			If resources.Length > 0 Then
+				Session("resourcesSelectCommand") = String.Format("SELECT [ID], [Model] FROM [Cars] WHERE [ID] IN ({0})", resources)
+			Else
+				Session("resourcesSelectCommand") = defaultResourcesSelectCommand
+			End If
+		End If
+
+		SqlDataSourceResources.SelectCommand = Session("resourcesSelectCommand").ToString()
+	End Sub
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not IsPostBack) Then
+			ASPxScheduler1.Start = New DateTime(2008, 7, 12)
+		End If
+	End Sub
+
+	Protected Sub ASPxScheduler1_BeforeExecuteCallbackCommand(ByVal sender As Object, ByVal e As SchedulerCallbackCommandEventArgs)
+		If e.CommandId.Contains("FLTRES") Then
+			ASPxScheduler1.ApplyChanges(ASPxSchedulerChangeAction.NotifyResourceIntervalChanged)
+		End If
+	End Sub
+
+	Protected Sub ASPxScheduler1_AppointmentRowInserting(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxScheduler.ASPxSchedulerDataInsertingEventArgs)
+		e.NewValues.Remove("ID")
+	End Sub
+
+	Protected Sub SqlDataSourceAppointments_Inserted(ByVal sender As Object, ByVal e As SqlDataSourceStatusEventArgs)
+		Dim connection As SqlConnection = CType(e.Command.Connection, SqlConnection)
+
+		Using command As New SqlCommand("SELECT IDENT_CURRENT('CarScheduling')", connection)
+			lastInsertedAppointmentId = Convert.ToInt32(command.ExecuteScalar())
+		End Using
+	End Sub
+
+	Protected Sub ASPxScheduler1_AppointmentRowInserted(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxScheduler.ASPxSchedulerDataInsertedEventArgs)
+		e.KeyFieldValue = lastInsertedAppointmentId
+	End Sub
+
+	Protected Sub ASPxScheduler1_AppointmentsInserted(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs)
+		Dim apt As Appointment = CType(e.Objects(0), Appointment)
+		Dim storage As ASPxSchedulerStorage = CType(sender, ASPxSchedulerStorage)
+		storage.SetAppointmentId(apt, lastInsertedAppointmentId)
+	End Sub
+End Class
